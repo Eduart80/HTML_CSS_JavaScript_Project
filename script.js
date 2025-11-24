45fc8b1bb0ae971ac113d9b0fad5bbf706dbd063
Eduart2021 <zeedymarina@yahoo.com>
2025-11-24 13:32:19 -0600

    setting up a card for each country

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 
-export async function fetchData() {
-   const respond = await fetch('./data.json')
-    const data = await respond.json()
-    return data
+
+const country = document.getElementById('country')
+const button = document.getElementById('searchBtn')
+const searchInput = document.getElementById('search')
+const img = document.getElementById('country-flag');
+
+button.addEventListener('click', () => {
+    let input = searchInput.value;
+    console.log(input);
+});
+
+async function countryData(params) {
+    const countryD = await fetchData(params)
+    country.textContent = `<li>${countryD[0].name.common}</li>`
+    countryD[0].name.common
+   
 }
+countryData('Albania')
+// get all
+// async function fetchData() {
+//     const respond = await fetch(`https://restcountries.com/v3.1/`);
+//     // need up to 10 fields
+//     const data = await respond.json();
+//     return data;
+// }
+// fetchData by id
+async function fetchData(param) {
+    const respond = await fetch(`https://restcountries.com/v3.1/name/${param}`);
+    const data = await respond.json();
+    return data;
+}
+
+// "name.common" for name
+//  "capital": 
+// "region"
+// "languages" "sqi"
+//  "flags":  "png": "https://flagcdn.com/w320/al.png",
+// flags     "svg": "https://flagcdn.com/al.svg",
+
+// "population":
+// "currencies""ALL": 
+// currencies "name": 
