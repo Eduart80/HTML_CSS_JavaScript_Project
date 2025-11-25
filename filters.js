ee066ee66a40cc41801b3d94c51e7bedc1f849c1
Eduart2021 <zeedymarina@yahoo.com>
2025-11-25 10:32:29 -0600

    moon light theme

--- filters.js
+++ filters.js
@@ -16,3 +16,12 @@ regionFilters.addEventListener('change', (event)=>{
         })
     })
 })
+document.getElementById('theme-toggle').addEventListener('click', function() {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+  } else {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+  }
+});
